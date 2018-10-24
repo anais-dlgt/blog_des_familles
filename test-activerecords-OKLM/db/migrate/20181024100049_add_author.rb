04e0b029399d8a6bfefb8edafe88a9c1e3f0a344
Anaïs <anais.diologeant@hotmail.fr>
2018-10-24 21:36:37 +0200

    Remove folder from repository

--- test-activerecords-OKLM/db/migrate/20181024100049_add_author.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthor < ActiveRecord::Migration[5.2]
-  def change
-    add_column :authors, :author_name, :string    
-  end
-end
--- test-activerecords-OKLM/db/migrate/20181024100847_add_book.rb
+++
@@ -1,5 +0,0 @@
-class AddBook < ActiveRecord::Migration[5.2]
-  def change
-    add_column :books, :book_name, :text
-  end
-end
